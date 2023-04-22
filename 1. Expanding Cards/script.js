8a8a4f1b1208aadcff142997bedf9120baacdf85
Balaji-Nandure <balajirn.2016@gmail.com>
2023-04-22 17:48:07 +0530

    project 1 done

---
+++ 1. Expanding Cards/script.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        // console.log("clicked");
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel, index) => {
+        panel.classList.remove("active");
+    });
+}
